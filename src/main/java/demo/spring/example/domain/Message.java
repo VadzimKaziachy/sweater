b225dea550fdff9c672037aef19e06e1364df477
Vadim Koziachiy <vadim.koziachiy@exposit.com>
2019-11-10 14:50:06 +0300

    added ability edit user profile and added migrations for database.

--- src/main/java/demo/spring/example/domain/Message.java
+++ src/main/java/demo/spring/example/domain/Message.java
@@ -7,7 +7,7 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -34,7 +34,7 @@ public class Message {
         this.tag = tag;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -51,7 +51,7 @@ public class Message {
         return this.tag;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return this.id;
     }
 
